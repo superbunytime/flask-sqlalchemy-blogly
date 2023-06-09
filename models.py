60f41411cfb70e34a378566c5c9a3fac8f6f55b5
Eric (Cody) <51343804+superbunytime@users.noreply.github.com>
2023-06-09 15:20:48 -0700

    wsl debian is wonky

--- models.py
+++ models.py
@@ -1,28 +1,55 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
 """Models for Blogly."""
+
 
 class User(db.Model):
     """User"""
     __tablename__ = "users"
 
     id = db.Column(db.Integer,
-    primary_key=True)
+                   primary_key=True)
 
     first_name = db.Column(db.Text,
-    nullable=False)
+                           nullable=False)
 
     last_name = db.Column(db.Text,
-    nullable=False)
+                          nullable=False)
 
     image_url = db.Column(db.Text,
-    nullable=False)
+                          nullable=False)
+
+    posts = db.relationship("Post", backref="user",
+                            cascade="all, delete-orphan")
 
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    """Post"""
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.now)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def formatted_date(self):
+        """get a formatted date"""
+        return self.created_at.strftime("%c")
+
 
 def connect_db(app):
     """Connect to database."""
